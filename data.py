1db8cd91502482234d0e4e922e7f8e53ffe296c1
LAPTOP-IE9QNBTN\Ben Hsieh <b10071007@hotmail.com>
2022-10-06 00:17:48 +0800

    Implement TimeSeriesDataset class

--- data.py
+++ data.py
@@ -1,8 +1,26 @@
 import os
 import numpy as np
 import pandas as pd
+import warnings
+from torch.utils.data import Dataset
+
+warnings.filterwarnings("ignore")
 
 #------------------------------------------------------------------------------------------#
+
+class TimeSeriesDataset(Dataset):
+    def __init__(self, x, y):
+        x = np.expand_dims(x, 2)
+        # y = np.expand_dims(y, 2)
+        self.x = x.astype(np.float32)
+        self.y = y.astype(np.float32)
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, idx):
+        return (self.x[idx], self.y[idx])
+
 
 class CycleMaker():
     def __init__(self):
@@ -17,10 +35,10 @@ class CycleMaker():
         drop_idx_list = []
         drop_x_list = []
         for idx in range(num_higher_left, num_y-num_higher_right+1):
-            yi = y[idx]
+            yi = y.loc[idx]
             if np.argmin(y[(idx-num_higher_left):(idx+num_higher_right+1)]) == num_higher_left:
                 drop_idx_list.append(idx)
-                drop_x_list.append(x[idx])
+                drop_x_list.append(x.loc[idx])
         return drop_idx_list, drop_x_list
     
     def get_ts_cycle_by_drop_point(self, x, y, drop_idx_list):
@@ -61,28 +79,100 @@ class CycleMaker():
             cycle_y = cycle_y_list[idx]
             cycle_idx = cycle_idx_list[idx]
             length = length_list[idx]
-            data.iloc[:length, 2*idx] = cycle_x
-            data.iloc[:length, 2*idx+1] = cycle_y
+            # if length==18:
+            #     break
+            data.iloc[:length, 2*idx] = cycle_x.to_numpy()
+            data.iloc[:length, 2*idx+1] = cycle_y.to_numpy()
+            # print(data.iloc[0, 2*idx])
+            # print(data.iloc[0, 2*idx+1])
             data = data.rename(columns={2*idx: f"{length}_{cycle_idx[0]}_{cycle_idx[1]}_time", 
                                         2*idx+1: f"{length}_{cycle_idx[0]}_{cycle_idx[1]}_value"})
             
         os.makedirs(os.path.split(dst_data_path)[0], exist_ok=True)
+        # print(data)
         data.to_csv(dst_data_path, index=False)
 
-def test_CycleMaker():
-    root_dir = "."
-    data_dir = os.path.join(root_dir, "raw_data", "tw_stock")
-    data_path = os.path.join(data_dir, "0050_202201_202209.csv")
-    dst_data_path = os.path.join(root_dir, "dataset", "0050_cycle.csv")
-    data = pd.read_csv(data_path)
-    data.to_csv(data_path, index=False)
-    x = data["date"]
-    y = data["closing"]
+def convert_to_train_data(value, num_input=2, num_output=2):
+    if type(value) is pd.Series:
+        value = value.to_numpy()
     
-    cycle_maker = CycleMaker()
-    cycle_maker.save_cycle_data(x, y, 3, 3, dst_data_path)
+    num_value = len(value)
+    x = []
+    y = []
+    for idx in range(num_value-num_input-num_output+1):
+        x.append(value[idx:idx+num_input])
+        y.append(value[idx+num_input:idx+num_input+num_output])
+    x = np.array(x)
+    y = np.array(y)
+    
+    return x, y
+    
+
+def prepare_cycle_data(data, num_input=2, num_output=2, train_ratio=0.7):
+    num_cycle = data.shape[1]//2
+    min_length = num_input + num_output
+    
+    idx_list_select = []
+    num_cycle_select = 0
+    for idx in range(num_cycle):
+        length, start_idx, end_idx = data.columns[2*idx].split("_")[:-1]
+        length = int(length)
+        # if idx==35:
+        #     break
+        if length>=min_length:
+            idx_list_select.append(idx)
+            num_cycle_select += 1
+            
+    idx_split = round(num_cycle_select*train_ratio)
+    
+    # train
+    train_x = []
+    train_y = []
+    for idx in idx_list_select[:idx_split]:
+        length, start_idx, end_idx = data.columns[2*idx].split("_")[:-1]
+        length = int(length)
+        # date = data.iloc[:length,2*idx]
+        value = data.iloc[:length,2*idx+1]
+        x, y = convert_to_train_data(value, num_input, num_output)
+        train_x.append(x)
+        train_y.append(y)
+        # if i == 2:
+        #     break
+
+    train_x = np.concatenate(train_x)
+    train_y = np.concatenate(train_y)
+
+    # val
+    val_x = []
+    val_y = []
+    for idx in idx_list_select[idx_split:]:
+        length, start_idx, end_idx = data.columns[2*idx].split("_")[:-1]
+        length = int(length)
+        # date = data.iloc[:length,2*idx]
+        value = data.iloc[:length,2*idx+1]
+        x, y = convert_to_train_data(value, num_input, num_output)
+        val_x.append(x)
+        val_y.append(y)
+        # if idx==35:
+        #     break
+    val_x = np.concatenate(val_x) 
+    val_y = np.concatenate(val_y)
+    
+    return train_x, train_y, val_x, val_y
+
+def test_TimeSeriesDataset():
+    root_dir = "."
+    data_path = os.path.join(root_dir, "dataset", "tw_stock", "0050_cycle_raw.csv")
+    data = pd.read_csv(data_path)
+    train_x, train_y, val_x, val_y = prepare_cycle_data(data, num_input=2, num_output=2, train_ratio=0.7)
+    
+    train_dataset = TimeSeriesDataset(train_x, train_y)
+    x, y = train_dataset[0:5]
+    
+    
+    
 
 #------------------------------------------------------------------------------------------#
 
 if __name__ == "__main__":
-    test_CycleMaker()
+    test_TimeSeriesDataset()
